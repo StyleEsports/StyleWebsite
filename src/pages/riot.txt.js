edd7e7b2c439e48c886e9b091c15a9f3a8e42763
WillGerber <54857867+WillGerber@users.noreply.github.com>
2021-08-07 18:02:12 -0700

    New Download Try

--- src/pages/riot.txt.js
+++ src/pages/riot.txt.js
@@ -18,20 +18,9 @@ export default () => {
         <html lang={lang || 'en'} />
         <meta name="description" content="Riot TXT Code" />
       </Helmet>
-      <section id="hero" className="jumbotron">
-        <Container>
-          <Fade bottom duration={1000} delay={500} distance="30px">
-            <h1 className="hero-title text-center">25915edc-3ca6-4b43-908b-7961b5de9ded</h1>
-          </Fade>
-          <Fade bottom duration={1000} delay={1000} distance="30px">
-            <p className="hero-cta justify-content-center">
-              <Link className="cta-btn cta-btn--hero" to="/">
-                Go back
-              </Link>
-            </p>
-          </Fade>
-        </Container>
-      </section>
+      <a href="/Riot.txt" download>
+        Click to download
+      </a>
     </>
   );
 };
